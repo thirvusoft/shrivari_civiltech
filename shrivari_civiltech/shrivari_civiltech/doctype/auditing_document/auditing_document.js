c34ca81e8a0112d62f1787b41b8e853c7af84bc8
creative-paramu <pparameshwari@thirvusoft.in>
2024-05-21 22:34:35 +0530

    auditing_document

---
+++ shrivari_civiltech/shrivari_civiltech/doctype/auditing_document/auditing_document.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Thirvusoft Private Limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Auditing Document", {
+// 	refresh(frm) {
+
+// 	},
+// });
